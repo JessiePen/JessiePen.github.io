f52862cc1e36c3ca82f6b435664c6e3b717ae101
JessiePen <y.peng1998@gmail.com>
2021-03-06 03:13:59 +0000

    Site updated: 2021-03-06 03:13:58

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -28,26 +28,6 @@ import { addNewClass, removeClass, throttle } from './class-module'
         )
     })
 
-    // 监听屏幕滚动修改边栏
-    window.addEventListener(
-        'scroll',
-        throttle(() => {
-            const _top =
-                document.documentElement.scrollTop || document.body.scrollTop
-            if (_top > 100) {
-                // 边栏绝对定位
-                addNewClass('.sidebar', 'sidebar-fixed')
-                // 返回顶部按钮显示
-                $('#backtop').fadeIn(300)
-            } else {
-                // 取消边栏定位
-                removeClass('.sidebar', 'sidebar-fixed')
-                // 返回顶部按钮消失
-                $('#backtop').fadeOut(300)
-            }
-        })
-    )
-
     // 代码高亮
     document.querySelectorAll('pre code').forEach((block) => {
         hljs.highlightBlock(block)
@@ -127,4 +107,148 @@ import { addNewClass, removeClass, throttle } from './class-module'
         $('.header-menu-mobile-menu').fadeOut(300)
         removeClass($('body'), 'mobile-menu-fixed')
     })
+
+    // 监听屏幕滚动修改边栏
+    window.addEventListener(
+        'scroll',
+        throttle(() => {
+            const _top =
+                document.documentElement.scrollTop || document.body.scrollTop
+            if (_top > 100) {
+                // 边栏绝对定位
+                addNewClass('.sidebar', 'sidebar-fixed')
+                // 返回顶部按钮显示
+                $('#backtop').fadeIn(300)
+            } else {
+                // 取消边栏定位
+                removeClass('.sidebar', 'sidebar-fixed')
+                // 返回顶部按钮消失
+                $('#backtop').fadeOut(300)
+            }
+        })
+    )
+
+    // Perfect Scrollbar
+    const _widget = document.querySelector('#widget')
+    const widget = new PerfectScrollbar(_widget)
+
+    // Typed
+    if (window.aomori_logo_typed_animated) {
+        const typed = new Typed('#typed', {
+            stringsElement: '#typed-strings',
+            fadeOut: true,
+            fadeOutDelay: 800,
+            typeSpeed: 100,
+            showCursor: false,
+        })
+    }
+
+    // Algolia
+    if (window.aomori_search_algolia) {
+        const _searchPs = document.querySelector('#search-ps')
+        const searchPs = new PerfectScrollbar(_searchPs)
+
+        const algoliaConfig = document.querySelector(
+            'meta[property="algolia:search"]'
+        ).dataset
+
+        const algoliaClient = algoliasearch(
+            algoliaConfig.applicationId,
+            algoliaConfig.apiKey
+        )
+        const algoliaIndex = algoliaClient.initIndex(algoliaConfig.indexName)
+
+        $('#search').on(
+            'keyup',
+            throttle((event) => {
+                algoliaIndex.search($('#search').val()).then(({ hits }) => {
+                    $('.search-result').slideDown()
+
+                    if (hits.length) {
+                        let searchOutputHtml = ''
+                        hits.forEach((item) => {
+                            searchOutputHtml += `<a class="search-result-item" href="${
+                                item.permalink
+                            }"><h1>${item.title}</h1><p>${dayjs(
+                                item.date
+                            ).format('YYYY-MM-DD')}</p></a>`
+                        })
+                        $('.search-result').html(searchOutputHtml)
+                    } else {
+                        $('.search-result').html('Nothing at all.')
+                    }
+                })
+            })
+        )
+        $('#search').on('focusin', () => {
+            addNewClass($('.search'), 'search-focus')
+        })
+        $('#search').on('focusout', () => {
+            removeClass($('.search'), 'search-focus')
+            $('.search-result').slideUp()
+        })
+    }
+
+    // Swiper
+    const mySwiper = new Swiper('.swiper-container', {
+        autoplay: {
+            delay: 10000,
+        },
+        autoHeight: true,
+        pagination: {
+            el: '.swiper-pagination',
+            bulletActiveClass: 'article-gallery-active',
+        },
+    })
+    if (mySwiper.slides && mySwiper.slides.length <= 1) {
+        mySwiper.destroy()
+    }
+
+    // Viewer
+    const viewerConfig = {
+        title: false,
+        toolbar: {
+            zoomIn: true,
+            zoomOut: true,
+            reset: true,
+            prev: true,
+            next: true,
+        },
+        keyboard: false,
+    }
+    const galleryViewer = $('.article-gallery')
+    if (galleryViewer && galleryViewer.length > 0) {
+        galleryViewer.viewer(viewerConfig)
+    }
+    const articleEntryViewer = $('.article-entry')
+    if (articleEntryViewer && articleEntryViewer.length > 0) {
+        articleEntryViewer.viewer(viewerConfig)
+    }
+
+    // Plyr
+    if (window.isPost) {
+        const plyrsInPost = Array.from($('article video')).map(
+            (ele) => new Plyr(ele)
+        )
+    }
+    const plyrsInIndex = Array.from($('article .article-video-plyr')).map(
+        (ele) => new Plyr(ele)
+    )
+
+    // LazyLoad
+    const lazyLoad = new LazyLoad()
+
+    // DisqusJS
+    if (window.aomori.disqusjs && window.aomori.disqusjs.enable) {
+        const dsqjs = new DisqusJS({
+            shortname: window.aomori.disqusjs.shortname,
+            siteName: window.aomori.disqusjs.siteName,
+            api: window.aomori.disqusjs.api,
+            apikey: window.aomori.disqusjs.apikey,
+            nesting: window.aomori.disqusjs.nesting,
+            nocomment: window.aomori.disqusjs.nocomment,
+            admin: window.aomori.disqusjs.admin,
+            adminLabel: window.aomori.disqusjs.adminLabel,
+        })
+    }
 })()
--- javascripts/gitalk.js
+++ javascripts/gitalk.js
@@ -1,6 +1,6 @@
 ;(function () {
-    if (window.aomori && window.aomori.gitalk.enable) {
-        console.log(window.aomori.gitalk)
+    if (window.aomori && window.aomori.gitalk) {
+        window.aomori.gitalk.id = md5(window.location.href)
         const gitalk = new Gitalk(window.aomori.gitalk)
         gitalk.render('gitalk-container')
     }
